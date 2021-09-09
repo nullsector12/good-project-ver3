afc1b7d5c4aa5275f91dd15461fb79f1ec04a911
SeungYeon Kim <rlamd0412@gmail.com>
2021-09-09 22:40:28 +0900

    initial commit

---
+++ src/main/java/com/ksy/tom/ver3/goodprojectver3/GoodProjectVer3Application.java
@@ -0,0 +1,13 @@
+package com.ksy.tom.ver3.goodprojectver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodProjectVer3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodProjectVer3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksy/tom/ver3/goodprojectver3/GoodProjectVer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.tom.ver3.goodprojectver3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodProjectVer3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
